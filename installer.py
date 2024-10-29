aee7d4f271e71f28b3e36decd88b7386c90fe626
Ati1707 <152104750+Ati1707@users.noreply.github.com>
2024-10-29 13:25:21 +0100

    added documentation target and fixed bug when overwriting existing files

--- installer.py
+++ installer.py
@@ -32,16 +32,16 @@ config.read("config.ini")
 library_path = config["PATH"]["LibraryPath"]
 
 if not library_path or library_path == "Y:\\example":
-    print("You need to set a asset library path in the config.ini")
-    print("Create a new folder don't use an existing folder")
-    print("This tool is not stable yet!!! Data loss can occur if you use an existing folder")
+    print(f"{Bcolors.WARNING}You need to set a asset library path in the config.ini{Bcolors.ENDC}")
+    print(f"{Bcolors.WARNING}Create a new folder don't use an existing folder{Bcolors.ENDC}")
+    print(f"{Bcolors.WARNING}This tool is not stable yet!!! Data loss can occur if you use an existing folder{Bcolors.ENDC}")
     sys.exit(0)
 
 # Define the target folders
 target_folders = [
     "aniBlocks", "data", "Environments", "Light Presets", "People",
     "Props", "ReadMe's", "Render Presets", "Render Settings", "Runtime",
-    "Scenes", "Scripts", "Shader Presets", "Cameras"
+    "Scenes", "Scripts", "Shader Presets", "Cameras", "Documentation"
 ]
 
 def clean_temp_folder():
@@ -53,24 +53,46 @@ def extract_archive(item):
         if item.endswith(('.zip', '.rar', '7z', '.tar')):
             print(f"Extracting: {item}")
             try:
-                patoolib.extract_archive(item_path, outdir=temp_folder, verbosity= -1)
+                patoolib.extract_archive(item_path, outdir=temp_folder, verbosity= -1, interactive=False)
                 time.sleep(1)
                 return True
             except:
                 print(f"{Bcolors.FAIL}The archive {item} can not be extracted{Bcolors.ENDC}")
                 return False
 
+def clean_folder():
+    targets = [folder for folder in target_folders]
+
+    # Walk through the directory
+    for item in os.listdir(temp_folder):
+        item_path = os.path.join(temp_folder, item)
+
+        # If it's a folder and not in target folders, delete it
+        if os.path.isdir(item_path) and item not in targets:
+            shutil.rmtree(item_path)
+            print(f"Deleted folder: {item_path}")
+
+        # If it's a file, delete it
+        elif os.path.isfile(item_path):
+            os.remove(item_path)
+            print(f"Deleted file: {item_path}")
+
+
 def traverse_directory(folder_path):
+    archive_extracted = False
     for root, dirs, files in os.walk(folder_path):
-        if any(target in dirs for target in target_folders):
-            shutil.copytree(root, library_path, dirs_exist_ok=True)
-            return True
         for file in files:
             if file.endswith(('.zip', '.rar', '7z', '.tar')):
-                patoolib.extract_archive(os.path.join(str(root), file), outdir=root, verbosity= -1)
-                time.sleep(1)
+                patoolib.extract_archive(os.path.join(str(root), file), outdir=root, verbosity= -1, interactive=False)
+                time.sleep(0.5)
                 os.remove(os.path.join(root, file))
-                return traverse_directory(folder_path)
+                archive_extracted = True
+        if archive_extracted:
+            return traverse_directory(folder_path)
+        if any(target in dirs for target in target_folders):
+            clean_folder()
+            shutil.copytree(root, library_path, dirs_exist_ok=True)
+            return True
     return False
 
 clean_temp_folder()
