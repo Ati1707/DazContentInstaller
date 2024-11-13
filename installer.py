41332a10e22e6f6efbdcc0e5036fa43a33d05379
Ati1707 <152104750+Ati1707@users.noreply.github.com>
2024-11-13 02:37:07 +0100

    replace os with pathlib which improved the performance and rewrote the whole installer to be compatible with the gui

--- installer.py
+++ installer.py
@@ -1,16 +1,18 @@
+import pathlib
 import sys
-import os
 import time
 import shutil
 import configparser
 import re
+import threading
 
 import patoolib
-
 from patoolib.util import PatoolError
 
 import content_database
 import patches # Needed to apply monkey patching
+from helper.file_operations import create_temp_folder, delete_temp_folder
+
 
 # Class to create colored output
 class Bcolors:
@@ -27,12 +29,16 @@ temp_folder = 'temp/'
 config = configparser.ConfigParser()
 config.read("config.ini")
 library_path = config["PATH"]["LibraryPath"]
+is_debug_mode = config["DEBUG"].getboolean("IsDebugMode")
+
+
+lock = threading.Lock()
 
 if getattr(sys, "frozen", False):
     base_path = sys._MEIPASS
 else:
-    base_path = os.path.dirname(__file__)
-seven_zip_path = os.path.join(base_path, "7z\\7z.exe")
+    base_path = pathlib.Path(__file__).parents
+seven_zip_path = pathlib.Path(str(base_path)).joinpath( "7z\\7z.exe")
 
 if not library_path or library_path == "example":
     print(f"{Bcolors.WARNING}You need to set a asset library path in the config.ini{Bcolors.ENDC}")
@@ -64,49 +70,49 @@ def get_relative_path(full_path):
 
 def clean_temp_folder():
     shutil.rmtree(temp_folder)
-    os.makedirs(temp_folder)
+    pathlib.Path(temp_folder).mkdir(parents=True, exist_ok=True)
 
 # Extract archives in the download folder and move content to temp folder
-def extract_archive(item, is_debug_mode):
-        item_path = os.path.join(download_folder, item)
-        if item.lower().endswith(('.zip', '.rar', '7z', '.tar')):
-            print(f"Extracting: {item}")
+def extract_archive(item_path, is_debug_mode):
+        base_item_name = pathlib.Path(item_path).name
+        if base_item_name.lower().endswith(('.zip', '.rar', '7z', '.tar')):
+            print(f"Extracting: {base_item_name}")
             try:
                 if is_debug_mode:
-                    patoolib.extract_archive(item_path, outdir=temp_folder, verbosity= 2, interactive=False, program=seven_zip_path)
+                    patoolib.extract_archive(item_path, outdir=temp_folder, verbosity= 2, interactive=False, program=str(seven_zip_path))
                 else:
-                    patoolib.extract_archive(item_path, outdir=temp_folder, verbosity= -1, interactive=False, program=seven_zip_path)
+                    patoolib.extract_archive(item_path, outdir=temp_folder, verbosity= -1, interactive=False, program=str(seven_zip_path))
                 time.sleep(1)
                 return True
             except PatoolError:
-                print(f"{Bcolors.FAIL}The archive {item} can not be extracted{Bcolors.ENDC}")
+                print(f"{Bcolors.FAIL}The archive {base_item_name} can not be extracted{Bcolors.ENDC}")
                 return False
 
 # Removes everything in temp folder that is not one of the target folders before importing to library
 def clean_folder(folder_path):
     targets = [folder for folder in target_folders]
 
-    for item in os.listdir(folder_path):
-        item_path = os.path.join(temp_folder, item)
+    for item_path in pathlib.Path(folder_path).iterdir():
+        item = item_path.name
 
         # If it's a folder and not in target folders, delete it
-        if os.path.isdir(item_path) and item not in targets:
+        if pathlib.Path(item_path).is_dir() and item not in targets:
             shutil.rmtree(item_path)
 
         # If it's a file, delete it
-        elif os.path.isfile(item_path):
-            os.remove(item_path)
+        elif pathlib.Path(item_path).is_file():
+            pathlib.Path(item_path).unlink()
 
 def add_to_database(root_path, item):
-    archive_name = item.split(".")[0]
+    archive_name = item.stem.split(".")[0]
     file_list = []
 
     # Traverse through all files in the directory
-    for item in os.listdir(root_path):
-        for root, dir, files in os.walk(os.path.join(root_path, item)):
+    for item in pathlib.Path(root_path).iterdir():
+        for root, dirs, files in pathlib.Path(item).walk():
             if files:
                 for file in files:
-                    file_list.append(os.path.join(get_relative_path(root), file))
+                    file_list.append(str((pathlib.Path(get_relative_path(str(root))).joinpath(file))))
     if content_database.archive_exist(archive_name, file_list):
         return True
     print(f"Archive '{archive_name}' added to the database with {len(file_list)} files.")
@@ -117,20 +123,21 @@ def add_to_database(root_path, item):
 # Searching the content of extracted archive for target folders
 def traverse_directory(folder_path, current_item, is_debug_mode):
     archive_extracted = False
-    for root, dirs, files in os.walk(folder_path):
+    for root, dirs, files in pathlib.Path(folder_path).walk():
         for file in files:
             if file.lower().endswith(('.zip', '.rar', '7z', '.tar')):
                 if is_debug_mode:
-                    patoolib.extract_archive(os.path.join(str(root), file), outdir=root, verbosity=2,
-                                             interactive=False, program=seven_zip_path)
+                    pathlib.Path(str(root)).joinpath(file)
+                    patoolib.extract_archive(str(pathlib.Path(str(root)).joinpath(file)), outdir=str(root), verbosity=2,
+                                             interactive=False, program=str(seven_zip_path))
                 else:
-                    patoolib.extract_archive(os.path.join(str(root), file), outdir=root, verbosity=-1,
-                                             interactive=False, program=seven_zip_path)
+                    patoolib.extract_archive(str(pathlib.Path(str(root)).joinpath(file)), outdir=str(root), verbosity=-1,
+                                             interactive=False, program=str(seven_zip_path))
                 time.sleep(0.5)
-                os.remove(os.path.join(root, file))
+                pathlib.Path(root).joinpath(file).unlink()
                 archive_extracted = True
         if archive_extracted:
-            return traverse_directory(folder_path, current_item)
+            return traverse_directory(folder_path, current_item, is_debug_mode)
         if any(target in dirs for target in target_folders):
             clean_folder(root)
             if add_to_database(root, current_item):
@@ -139,20 +146,20 @@ def traverse_directory(folder_path, current_item, is_debug_mode):
             return True
     return False
 
-def start_installer(is_debug_mode=False):
-    clean_temp_folder()
-    for current_item in os.listdir(download_folder):
-        if not extract_archive(current_item, is_debug_mode):
+def start_installer_gui(file_path, is_delete_archive=False):
+
+    with lock:
+        create_temp_folder()
+        clean_temp_folder()
+        if not extract_archive(file_path, is_debug_mode):
             clean_temp_folder()
-            continue
-        if traverse_directory(temp_folder, current_item, is_debug_mode):
-            print(f"{Bcolors.OKGREEN}{current_item} was successfully imported to your library{Bcolors.ENDC}")
+            return
+        if traverse_directory(temp_folder, pathlib.Path(file_path), is_debug_mode):
+            print(f"{Bcolors.OKGREEN}{file_path} was successfully imported to your library{Bcolors.ENDC}")
         else:
-            print(
-                f"{Bcolors.WARNING}{current_item} can not be automatically imported because it doesn't have the right folder structure{Bcolors.ENDC}")
+            print(f"{Bcolors.WARNING}{file_path} can not be automatically imported because it doesn't have the right folder structure{Bcolors.ENDC}")
             print(f"{Bcolors.WARNING}or the archive already exists in the database{Bcolors.ENDC}")
         clean_temp_folder()
-
-    print("Finished importing every asset")
-    print("Tool exiting in 3 seconds....")
-    time.sleep(3)
+        delete_temp_folder()
+        if is_delete_archive:
+            pathlib.Path(file_path).unlink()
