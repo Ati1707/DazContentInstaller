bdb813dfdf8ceba22953397d37a8717ee15b6b81
Ati1707 <152104750+Ati1707@users.noreply.github.com>
2025-02-02 23:46:01 +0100

    log and ignore request exceptions to github

--- helper/updater.py
+++ helper/updater.py
@@ -3,19 +3,22 @@ import urllib.request
 import webbrowser
 
 from packaging.version import Version
+from helper.file_operations import create_logger
 
 api_url = "https://api.github.com/repos/Ati1707/DazContentInstaller/releases/latest"
-
+logger = create_logger()
 
 def is_new_update_available(local_version):
-    response = urllib.request.urlopen(api_url).read()
-    data = json.loads(response)
+    try:
+        response = urllib.request.urlopen(api_url).read()
+        data = json.loads(response)
+        latest_version = data["tag_name"].strip("v")
+        return Version(latest_version) > Version(local_version)
+    except Exception as e:
+        # Handle any HTTP or network-related errors here
+        logger.warning(f"Could not check for updates. Reason: {e}")
+        return False
 
-    latest_version = data["tag_name"].strip("v")
-
-    if Version(latest_version) > Version(local_version):
-        return True
-    return False
 
 
 def open_release_page():
